48738eb22edce8d297d426b4fbc9d321b09d1910
Farhanrezy <farhanrezy70@gmail.com>
2023-10-30 09:27:56 +0700

    Percobaan 1

---
+++ ArrayBilangan19.java
@@ -0,0 +1,16 @@
+package Jobsheet_9;
+import java.util.Scanner;;
+
+public class ArrayBilangan19 {
+        public static void main (String[] args){
+            int [] bil = {5, 12867, 7, 2000000};
+
+    
+            System.out.println(bil[0]);
+            System.out.println(bil[1]);
+            System.out.println(bil[2]);
+            System.out.println(bil[3]);
+    
+        }
+    }
+
