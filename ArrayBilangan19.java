f47378e9abfa1cf528e31372579b855caa05bd50
Farhanrezy <farhanrezy70@gmail.com>
2023-10-30 09:41:39 +0700

    Modifikasi Percobaan 1

--- ArrayBilangan19.java
+++ ArrayBilangan19.java
@@ -4,12 +4,14 @@ import java.util.Scanner;;
 public class ArrayBilangan19 {
         public static void main (String[] args){
             int [] bil = {5, 12867, 7, 2000000};
-
+            for (int i = 0; i <= 4; i++){
+                System.out.println(bil[i]);
+            }
     
-            System.out.println(bil[0]);
-            System.out.println(bil[1]);
-            System.out.println(bil[2]);
-            System.out.println(bil[3]);
+            //System.out.println(bil[0]);
+            //System.out.println(bil[1]);
+            //System.out.println(bil[2]);
+            //System.out.println(bil[3]);
     
         }
     }
