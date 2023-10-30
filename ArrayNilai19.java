592a224502abceba140eda05bc2ee6b79216c801
Farhanrezy <farhanrezy70@gmail.com>
2023-10-30 10:36:10 +0700

    Modifikasi Percobaan 2

--- ArrayNilai19.java
+++ ArrayNilai19.java
@@ -9,14 +9,17 @@ public class ArrayNilai19 {
 
         int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++){
-            System.out.print("Masukkan Nilai akhir ke-"+1+" : ");
+        for (int i = 0; i <nilaiAkhir.length; i++){
+            System.out.print("Masukkan Nilai akhir ke-"+i+" : ");
 
             nilaiAkhir[i] = input.nextInt();
             
         }
         for (int i = 0; i < 10; i++){
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa ke-"+i+" Lulus ! ");
+            }else
+                System.out.println("Mahasiswa ke-"+i+" Tidak Lulus ! ");
         }
     }
     
